ea23d43c0b86a54fb50d73a07f7808dcd7c4cc22
Danilo Silva <danilo.silva@msn.com>
2021-10-11 23:25:48 -0300

    Implementação de métodos do Usuario

--- GerenciadorCondominios/GerenciadorCondominios.DAL/Interfaces/IUsuarioRepositorio.cs
+++ GerenciadorCondominios/GerenciadorCondominios.DAL/Interfaces/IUsuarioRepositorio.cs
@@ -1,8 +1,14 @@
 ﻿using GerenciadorCondominios.BLL;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL
 {
     public interface IUsuarioRepositorio : IRepositorioGenerico<Usuario>
     {
+        int VerificarSeExisteRegistro();
+        Task EfetuarLogon(Usuario usuario, bool lembrarDoLogin);
+        Task<IdentityResult> CriarUsuario(Usuario usuario, string senha);
+        Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao);
     }
 }
--- GerenciadorCondominios/GerenciadorCondominios.DAL/Repositorios/UsuarioRepositorio.cs
+++ GerenciadorCondominios/GerenciadorCondominios.DAL/Repositorios/UsuarioRepositorio.cs
@@ -1,11 +1,70 @@
 ﻿using GerenciadorCondominios.BLL;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL
 {
     public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
     {
-        public UsuarioRepositorio(Contexto contexto) : base(contexto)
+        private readonly Contexto contexto;
+        private readonly UserManager<Usuario> gerenciadorUsuarios;
+        private readonly SignInManager<Usuario> gerenciadorLogin;
+
+        public UsuarioRepositorio(Contexto contexto, UserManager<Usuario> gerenciadorUsuarios, SignInManager<Usuario> gerenciadorLogin) : base(contexto)
         {
+            this.contexto = contexto;
+            this.gerenciadorUsuarios = gerenciadorUsuarios;
+            this.gerenciadorLogin = gerenciadorLogin;
+        }
+
+        public async Task<IdentityResult> CriarUsuario(Usuario usuario, string senha)
+        {
+            try
+            {
+                return await this.gerenciadorUsuarios.CreateAsync(usuario, senha);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task EfetuarLogon(Usuario usuario, bool lembrarDoLogin)
+        {
+            try
+            {
+                await this.gerenciadorLogin.SignInAsync(usuario, lembrarDoLogin);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao)
+        {
+            try
+            {
+                await this.gerenciadorUsuarios.AddToRoleAsync(usuario, funcao);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int VerificarSeExisteRegistro()
+        {
+            try
+            {
+                return this.contexto.Usuarios.Count();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
